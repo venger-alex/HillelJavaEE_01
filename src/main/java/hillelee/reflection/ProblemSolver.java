d93f6f0f6e9ec07c0369a4113b31bced6220d19f
venger-alex <venger.alex@gmail.com>
2017-12-04 14:50:05 +0200

    Changes - method solve in the class ProblemSolver after viewing the next lesson video

--- src/main/java/hillelee/reflection/ProblemSolver.java
+++ src/main/java/hillelee/reflection/ProblemSolver.java
@@ -2,9 +2,12 @@ package hillelee.reflection;
 
 import lombok.SneakyThrows;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class ProblemSolver {
@@ -27,26 +30,38 @@ public class ProblemSolver {
     }
 */
 
+    //@SneakyThrows
     public String solve(Object problem) {
-        Class<?> aClass = problem.getClass();
-        Method[] methods = aClass.getMethods();
-
-        String s = Arrays.stream(methods)
+        return Stream.of(problem)
+                .map(Object::getClass)
+                .flatMap(clazz -> Arrays.stream(clazz.getMethods()))
                 .filter(method -> method.isAnnotationPresent(CorrectAnswer.class))
                 //.map(method -> (String) method.invoke(problem))
-                /**
+                /*
                  * Вынес invoke в отдельный приватный метод, а над ним уже
                  * поставил SneakyThrows и уже его использую в лямбде,
                  * но не уверен, что нет получше вариантов или есть?))
                  */
                 .map(method -> invokeMethod(problem, method))
-                .collect(Collectors.toList()).get(0);
-
-        return s;
+                // Вариант Макса
+                //.map(invokeOn(problem))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Annotation CorrectAnswer not found"));
     }
 
     @SneakyThrows
     private String invokeMethod(Object object, Method method) {
         return (String) method.invoke(object);
+    }
+
+    // Вариант Макса
+    private Function<Method, String> invokeOn(Object obj) {
+        return method -> {
+            try {
+                return (String) method.invoke(obj);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
