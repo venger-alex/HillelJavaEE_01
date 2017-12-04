50cb49a1848937f5fedd5cf622fc0452a882e0b4
venger-alex <venger.alex@gmail.com>
2017-12-04 16:18:46 +0200

    Spring-context intro

--- src/main/java/hillelee/knight/FairyTale.java
+++ src/main/java/hillelee/knight/FairyTale.java
@@ -1,9 +1,11 @@
 package hillelee.knight;
 
-import lombok.Data;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
+        import lombok.Data;
+        import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+        import org.springframework.context.ApplicationContext;
+        import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+        import org.springframework.context.annotation.Scope;
+        import org.springframework.stereotype.Component;
 
 public class FairyTale {
     public static void main(String[] args) {
@@ -12,16 +14,21 @@ public class FairyTale {
 
         ApplicationContext ctx = new AnnotationConfigApplicationContext("hillelee");
 
-        System.out.println(ctx.getBean("knight"));
-        System.out.println(ctx.getBean(Knight.class));
-        System.out.println(new Knight(new Quest()));
-
+//        System.out.println(ctx.getBean("knight"));
+//        System.out.println(ctx.getBean(Knight.class));
+//        System.out.println(new Knight(new Quest()));
+//        System.out.println(ctx.getBean("myKnight"));
+        Knight knight1 = ctx.getBean(Knight.class);
+        Knight knight2 = ctx.getBean(Knight.class);
+        System.out.println("Knights are same: " + (knight1 == knight2));
+        System.out.println("Quests are same: " + (knight1.getQuest() == knight2.getQuest()));
 
     }
 }
 
 @Data
-@Component
+@Component("myKnight")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 class Knight {
     private final Quest quest;
 }
